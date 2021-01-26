fd759c9939566557efdeae446ff86df7b0b685df
AIvanov <isanioka@gmail.com>
2021-01-26 19:29:54 +0300

    Fixed FlatList warning

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -10,6 +10,7 @@ const CarsList = (props) => {
       <FlatList
         data={cars}
         renderItem={({item}) => <CarItem car={item} /> }
+        keyExtractor={(item, index) => index.toString()}
         showsVerticalScrollIndicator={false}
         snapToAlignment={'start'}
         decelerationRate={'fast'}
